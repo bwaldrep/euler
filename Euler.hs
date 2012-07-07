ba58d1f1fbcd836be83c5d83e3e37d828656e9cc
Bill Waldrep <bill.waldrep@gmail.com>
2012-07-06 21:37:59 -0500

    got problem 34

--- Euler.hs
+++ Euler.hs
@@ -1,5 +1,26 @@
-l = [1..1000]
+-- Project euler problems in Haskell
 
-f :: Integer -> Integer
-f n = n ^ n
+module Euler where
 
+import Data.Char
+
+-- Problem 48
+p48 :: Int -> Int
+p48 n = sum $ map (\x -> x ^ x) [1..n]
+
+-- Problem 16
+splitInt :: Int -> [Int]
+splitInt n = map ((\x -> x - 48) . ord) $ show n
+
+p16 :: Int -> Int
+p16 n = sum $ splitInt (2^n)
+
+-- Problem 34
+fact :: Int -> Int
+fact n = product [1..n]
+
+sumfact :: Int -> Bool
+sumfact n = n == (sum $ map fact $ splitInt n)
+
+p34 :: Int -> Int
+p34 n = sum $ filter sumfact [4..n]
