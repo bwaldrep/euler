8c4023c1b44fbf1122b32bb9c0589e27e3e05c00
Bill Waldrep <bill.waldrep@gmail.com>
2012-07-14 16:40:50 -0500

    got problem 21

--- Euler.hs
+++ Euler.hs
@@ -2,14 +2,17 @@
 
 module Euler where
 
+import Data.Array
 import Data.Char
 import Data.List
+import qualified Data.Map as Map
 
 -- Problem 48
 p48 :: Int -> Int
 p48 n = sum $ map (\x -> x ^ x) [1..n]
 
 -- Problem 16
+-- split an integer into a list of its digits in base 10
 splitInt :: Int -> [Int]
 splitInt n = map digitToInt $ show n
 
@@ -20,6 +23,7 @@ p16 n = sum $ splitInt (2^n)
 fact :: Int -> Int
 fact n = product [1..n]
 
+-- compute the sum of the factorial of each digit in n
 sumfact :: Int -> Bool
 sumfact n = n == (sum $ map fact $ splitInt n)
 
@@ -27,19 +31,22 @@ p34 :: Int -> Int
 p34 n = sum $ filter sumfact [4..n]
 
 -- Problem 21
-
 -- compute the list of proper divisors of n
 divisors :: Int -> [Int]
-divisors n = filter (\x -> rem n x == 0) [1.. div n 2]
+divisors n = filter (\x -> 0 == rem n x) [1.. div n 2]
 
-amicable :: (Int, Int) -> Bool
-amicable (a,b) = (sum (divisors a)) == (sum ( divisors b))
+-- creates an array of size n, where the i'th value is the sum of i's divisors
+divarray :: Int -> Array Int Int
+divarray n = array (1,n) [(i,(sum . divisors) i) | i <- [1..n]]
 
-split :: (Int, Int) -> [Int]
-split (a,b) = [a,b]
+-- sum all amicable pairs less than n
+p21 n = sum [a + b | a <- [2..n-1], let b = divs ! a, amicable a b]
+      where divs = divarray n
+            amicable a b = a < b && b < n && divs ! b == a
 
-getamicable :: Int -> [Int]
-getamicable n = nub . concat . map split $ filter amicable [(a,b) | a <- [1..n], b <- [1..n], a<b]
+-- Problem 12
+-- Far too slow, need to redo
+-- list of all triangle numbers
+--trinums = scanl (+) 1 [2..]
 
-p21 :: Int -> Int
-p21 = sum . getamicable
+--p12 = head $ filter (\x -> (length . divisors) x > 500) trinums
