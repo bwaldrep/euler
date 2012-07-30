0f108b1c55f9c1211ce42f73550ffaf9f24a0ee6
Bill Waldrep <wwaldrep@enova.com>
2012-07-30 00:05:22 -0500

    solved problem 3 in c++

---
+++ problem3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+typedef long long int lli;
+typedef vector<bool> table;
+
+lli compFactor(lli num);
+lli firstFactor(lli num);
+void buildTable(lli cap, table &lookup);
+
+int main() {
+  lli bignum = 600851475143;
+  cout << "ANSWER: " << compFactor(bignum) << endl;
+  return 0;
+}
+
+lli compFactor(lli num) {
+  lli ans = 1;
+  lli current = num;
+  while(ans = firstFactor(current)) {
+    current = current / ans;
+  }
+  return current;
+}
+
+lli firstFactor(lli num) {
+  table lookup;
+  buildTable(sqrt(num), lookup);
+  lli size = lookup.size();
+  for(lli i = 3; i < size; i += 2) {
+    if (lookup[i] && (num % i == 0)) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+void buildTable(lli cap, table &lookup) {
+  lookup.resize(cap);
+  lookup[0] = lookup[1] = false;
+  for(lli i = 2; i < cap; i++) {
+    lookup[i] = true;
+  }
+  for(lli i = 2; i < cap; i++) {
+    if(lookup[i]) {
+      for(lli j = i * 2; j < cap; j += i) {
+        lookup[j] = false;
+      }
+    }
+  }
+}
