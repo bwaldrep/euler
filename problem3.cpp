9b1fa9a7757f21f92c64eb275316452f51bab7a4
Bill Waldrep <wwaldrep@enova.com>
2012-07-30 00:51:34 -0500

    fixed typos

--- problem3.cpp
+++ problem3.cpp
@@ -19,10 +19,10 @@ lli compFactor(lli num) {
   // Composite numbers have at least one prime factor
   // less than their square root
   lli cap = sqrt(num);
-  // initialize lookup vector<bool>
+  // Initialize lookup table
   vector<bool> lookup;
   buildTable(cap,lookup);
-  // find the first prime factor, divide it from the starting
+  // Find the first prime factor, divide it from the starting
   // number and repeat. If the result is composite it will also
   // have at least one prime factor less than cap. If it doesn't
   // then it is prime and we are done.
@@ -30,14 +30,14 @@ lli compFactor(lli num) {
   for(lli i = 2; i < cap; i++) {
     if(lookup[i] && (i < current) && (current % i == 0)) {
       current = current / i;
-      i--; // the sequence of prime factors may contain repeats
+      i--; //the sequence of prime factors may contain repeats
     }
   }
   return current;
 }
 
 void buildTable(lli cap, vector<bool> &lookup) {
-  // Build a lookup vector<bool> of all primes less
+  // Build a lookup table of all primes less
   // than cap using a basic sieve
   lookup.resize(cap);
   lookup[0] = lookup[1] = false;
